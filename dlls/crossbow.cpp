967279ce52dbbfab845a58fb4538c5375bfd7d03
devtunisia12 <94746514+devtunisia12@users.noreply.github.com>
2022-01-04 21:06:02 +0100

    Add files via upload

--- dlls/crossbow.cpp
+++ dlls/crossbow.cpp
@@ -472,7 +472,7 @@ void CCrossbow::Reload( void )
 		SecondaryAttack();
 	}
 
-	if ( DefaultReload( 5, CROSSBOW_RELOAD, 4.5 ) )
+	if ( DefaultReload( 1, CROSSBOW_RELOAD, 0.5 ) )
 	{
 		EMIT_SOUND_DYN(ENT(m_pPlayer->pev), CHAN_ITEM, "weapons/xbow_reload1.wav", RANDOM_FLOAT(0.95, 1.0), ATTN_NORM, 0, 93 + RANDOM_LONG(0,0xF));
 	}
